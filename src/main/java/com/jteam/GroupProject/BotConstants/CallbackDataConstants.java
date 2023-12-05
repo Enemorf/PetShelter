893be6509776d48341f0551c6354ec71c49fad7c
Aleksandr <a-katushnii@yandex.ru>
2023-12-05 22:47:13 +0300

    Реализовал кнопки этапа 2 и этапа 3. Добавил отдельный класс CallbackDataConstants, в котором хранятся CallbackData для кнопок

---
+++ src/main/java/com/jteam/GroupProject/BotConstants/CallbackDataConstants.java
@@ -0,0 +1,27 @@
+package com.jteam.GroupProject.BotConstants;
+
+public class CallbackDataConstants {
+    public static final String CALLBACK_DOG_SHELTER = "DOG_SHELTER";
+    public static final String CALLBACK_CAT_SHELTER = "CAT_SHELTER";
+    public static final String CALLBACK_INFO_SHELTER = "INFO_SHELTER";
+    public static final String CALLBACK_TAKE_ANIMAL_FROM_SHELTER = "TAKE_ANIMAL_FROM_SHELTER";
+    public static final String CALLBACK_REPORT = "REPORT";
+    public static final String CALLBACK_CALL_VOLUNTEER = "CALL_VOLUNTEER";
+    public static final String CALLBACK_INFO = "INFO";
+    public static final String CALLBACK_SCHEDULE = "SCHEDULE";
+    public static final String CALLBACK_SECURITY_CONTACTS = "SECURITY_CONTACTS";
+    public static final String CALLBACK_SAFETY_PRECAUTIONS = "SAFETY_PRECAUTIONS";
+    public static final String CALLBACK_CONTACTS = "CONTACTS";
+    public static final String CALLBACK_RULES = "RULES";
+    public static final String CALLBACK_DOCUMENT_LIST = "DOCUMENT_LIST";
+    public static final String CALLBACK_TRANSPORT_PET = "TRANSPORT_PET";
+    public static final String CALLBACK_HOME_IMPROVEMENT_FOR_SMALL_PET = "HOME_IMPROVEMENT_FOR_SMALL_PET";
+    public static final String CALLBACK_HOME_IMPROVEMENT_FOR_ADULT_PET = "HOME_IMPROVEMENT_FOR_ADULT_PET";
+    public static final String CALLBACK_HOME_IMPROVEMENT_FOR_PET_WITH_LIMITED_OPPORTUNITIES = "HOME_IMPROVEMENT_FOR_PET_WITH_LIMITED_OPPORTUNITIES";
+    public static final String CALLBACK_DOG_HANDLER_ADVICE = "DOG_HANDLER_ADVICE";
+    public static final String CALLBACK_DOG_HANDLER_LIST = "DOG_HANDLER_LIST";
+    public static final String CALLBACK_REASONS_FOR_REFUSAL = "REASONS_FOR_REFUSAL";
+    public static final String CALLBACK_REPORT_FORM = "REPORT_FORM";
+    public static final String CALLBACK_PASS_REPORT = "PASS_REPORT";
+
+}
